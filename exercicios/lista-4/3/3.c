b67d5d3a6888fc5804eb8dd4fab46cf9a19fd20a
Matheus Siqueira <siqueira4503@gmail.com>
2021-09-03 22:47:26 -0300

    exercicio 3

--- exercicios/lista-4/3/3.c
+++ exercicios/lista-4/3/3.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool primo(int n) {
+    int i;
+    int quantPrimo = 0;
+    for(i = 1; i <= n; i++) {
+        if(n%i == 0) {
+            quantPrimo++;
+        }
+    }
+    if(quantPrimo == 2) {
+        return true;
+    }
+    return false;
+}
+
 main() {
-    float n;
+    int numero;
+    bool resp;
     printf("Informe o numero: ");
-    scanf("%f", &n);
+    scanf("%d", &numero);
+    resp = primo(numero);
+    if(resp == true) {
+        printf("E primo");
+    } else {
+        printf("Nao eh primo");
+    }
 }
