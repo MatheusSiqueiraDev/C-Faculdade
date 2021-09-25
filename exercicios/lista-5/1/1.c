0cc8f24397f0f81e055759b09eca068238f9a0c7
Matheus Siqueira <siqueira4503@gmail.com>
2021-09-25 18:41:33 -0300

    Vetores

---
+++ exercicios/lista-5/1/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void inversor1() {
+
+}
+
+void inversor2() {
+
+}
+
+main() {
+    const int tamanho = 5;
+    int vetor[tamanho];
+    for(int i = 0; i <= tamanho; i++) {
+        vetor[i] = tamanho + i;
+    }
+    
+}
